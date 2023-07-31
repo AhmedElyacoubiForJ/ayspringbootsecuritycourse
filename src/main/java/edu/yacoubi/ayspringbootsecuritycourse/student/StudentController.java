3d0add3d15722b7b7aca8cdffe2deed516f8ae2b
A. El Yacoubi education <sihmed.elyacoubi@gmail.com>
2023-07-31 14:09:00 +0200

    added rest endpoint example

---
+++ src/main/java/edu/yacoubi/ayspringbootsecuritycourse/student/StudentController.java
@@ -0,0 +1,24 @@
+package edu.yacoubi.ayspringbootsecuritycourse.student;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1/students")
+public class StudentController {
+    
+    private static final List<Student> STUDENTS = List.of(
+            new Student(1, "James Bond"),
+            new Student(2, "Maria Jones"),
+            new Student(3, "Anna Smith")
+    );
+    
+    @GetMapping(path = "{studentId}")
+    public Student getStudent(@PathVariable("studentId") Integer studentId) {
+        return STUDENTS.stream()
+                .filter(student -> studentId.equals(student.getStudentId()))
+                .findFirst()
+                .orElseThrow(() -> new IllegalStateException("Student: " + studentId +" not found "));
+    }
+}
