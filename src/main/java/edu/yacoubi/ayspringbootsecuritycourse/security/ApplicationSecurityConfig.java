8385c829568c56e6498036900ddc6fe0dd741c55
A. El Yacoubi education <sihmed.elyacoubi@gmail.com>
2023-08-02 08:27:25 +0200

    password encoding implemented & other users added

--- src/main/java/edu/yacoubi/ayspringbootsecuritycourse/security/ApplicationSecurityConfig.java
+++ src/main/java/edu/yacoubi/ayspringbootsecuritycourse/security/ApplicationSecurityConfig.java
@@ -1,5 +1,6 @@
 package edu.yacoubi.ayspringbootsecuritycourse.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -8,11 +9,19 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
 public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public ApplicationSecurityConfig(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -32,12 +41,26 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
     protected UserDetailsService userDetailsService() {
         UserDetails annaSmithUser = User.builder()
                 .username("annasmith")
-                .password("1234")
+                .password(passwordEncoder.encode("password"))
                 .roles("STUDENT") // spring internally: ROLE_STUDENT
                 .build();
 
+        UserDetails lindaUser = User.builder()
+                .username("linda")
+                .password(passwordEncoder.encode("password123"))
+                .roles("ADMIN") // ROLE_ADMIN
+                .build();
+
+        UserDetails tomUser = User.builder()
+                .username("tom")
+                .password(passwordEncoder.encode("password123"))
+                .roles("ADMINTRAINEE") // ROLE_ADMINTRAINEE
+                .build();
+
         return new InMemoryUserDetailsManager(
-                annaSmithUser
+                annaSmithUser,
+                lindaUser,
+                tomUser
         );
     }
 }
