5fcc2770b662c8ff71d687ae0653faa35ce18930
A. El Yacoubi education <sihmed.elyacoubi@gmail.com>
2023-09-05 11:30:40 +0200

    own implementation from UserDetails instead of user.builder (refactoring)

---
+++ src/main/java/edu/yacoubi/ayspringbootsecuritycourse/auth/ApplicationUser.java
@@ -0,0 +1,71 @@
+package edu.yacoubi.ayspringbootsecuritycourse.auth;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+public class ApplicationUser implements UserDetails {
+
+    private final List<? extends GrantedAuthority> grantedAuthorities;
+
+    public ApplicationUser(List<? extends GrantedAuthority> grantedAuthorities,
+                           String password,
+                           String username,
+                           Boolean isAccountNonExpired,
+                           Boolean isAccountNonLocked,
+                           Boolean isCredentialsNonExpired,
+                           Boolean isEnabled) {
+        this.grantedAuthorities = grantedAuthorities;
+        this.password = password;
+        this.username = username;
+        this.isAccountNonExpired = isAccountNonExpired;
+        this.isAccountNonLocked = isAccountNonLocked;
+        this.isCredentialsNonExpired = isCredentialsNonExpired;
+        this.isEnabled = isEnabled;
+    }
+
+    private final String password;
+    private final String username;
+    private final Boolean isAccountNonExpired;
+    private final Boolean isAccountNonLocked;
+    private final Boolean isCredentialsNonExpired;
+    private final Boolean isEnabled;
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return grantedAuthorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return isAccountNonExpired;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return isAccountNonLocked;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return isCredentialsNonExpired;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return isEnabled;
+    }
+}
--- src/main/java/edu/yacoubi/ayspringbootsecuritycourse/security/ApplicationSecurityConfig.java
+++ src/main/java/edu/yacoubi/ayspringbootsecuritycourse/security/ApplicationSecurityConfig.java
@@ -1,12 +1,9 @@
 package edu.yacoubi.ayspringbootsecuritycourse.security;
 
-import static edu.yacoubi.ayspringbootsecuritycourse.security.ApplicationUserPermission.COURSE_READ;
-import static edu.yacoubi.ayspringbootsecuritycourse.security.ApplicationUserPermission.COURSE_WRITE;
 import static edu.yacoubi.ayspringbootsecuritycourse.security.ApplicationUserRole.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -16,7 +13,6 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import java.util.concurrent.TimeUnit;
